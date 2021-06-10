4bbd1cc7aa2cbf80b4103f6fc127ae084ce38a98
dashahello <st.daryaa@gmail.com>
2021-06-10 11:59:58 +0300

    basic set up

---
+++ src/components/Launches.js
@@ -0,0 +1,37 @@
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
+
+const LAUNCHES_QUERY = gql`
+  query LaunchesQuery {
+    launches {
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
+      tentative_max_precision
+    }
+  }
+`;
+
+export default function Launches() {
+  return (
+    <>
+      <h1 className="display-4 my-3">Launches</h1>
+      <Query query={LAUNCHES_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <h4>Loading...</h4>;
+          if (error) console.log(error);
+
+          return (
+            <>
+              {data.launches.map((launch) => (
+                <LaunchItem key={launch.flight_number} launch={launch} />
+              ))}
+            </>
+          );
+        }}
+      </Query>
+    </>
+  );
+}
